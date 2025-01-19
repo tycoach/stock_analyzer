7a732b0d14685adff6a823c12f955e121c3d09eb
tycoach <davidtaiwo56@gmail.com>
2025-01-19 17:39:01 +0100

    modified req and etl_pipeline

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 
 # Configurations
-API_KEY = os.getenv('12_API_KEY')
+API_KEY = os.getenv('API_KEY')
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
